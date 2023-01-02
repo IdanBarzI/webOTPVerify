c5327934a1090c911525459b40f5a843cc8a8696
Idan Barzilay <Idan.barz@gmail.com>
2023-01-02 09:35:16 +0200

    deploy

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React,{useEffect,useState} from 'react';
+
+import './App.css';
+
+const App = () => { 
+
+  const [otpCode, setOtpCode] = useState("_ _ _ _");
+  
+  useEffect(() => {
+
+    if ("OTPCredential" in window) {
+      const ac = new AbortController();
+    
+      navigator.credentials
+        .get({
+          otp: { transport: ["sms"] },
+          signal: ac.signal
+        })
+        .then((otp) => {
+          setOtpCode(otp);
+          ac.abort();
+        })
+        .catch((err) => {
+          ac.abort();
+          console.log(err);
+        });
+    }
+  },[])
+
+  return (
+    <div className="App">
+    <h1>Auto verify OTP on the web POC</h1>
+    <div>
+      Send an SMS that includes
+      <p><code>@www.IdanBarzi.github.io/webOTPVerify/ #1234</code></p>
+      to this phone.
+      <p><code>here @www.IdanBarzi.github.io/webOTPVerify/ is the domain from otp verification is to be done and  #1234 is the OTP</code></p  >
+      to this phone.
+    </div>
+    <br/>
+    <br/>
+    <br/>
+    <div style={{textAlign:"center"}}>
+       <h2> Your OTP:	&nbsp;
+        {otpCode}</h2>
+    </div>
+    </div>
+  );
+}
+
+export default App;
